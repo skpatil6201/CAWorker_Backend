bf6cd6a7c2686a212ebaf683ac4de2379a88b0fa
skpatil6201 <tejaspatil77777@hmail.com>
2025-12-11 22:39:20 +0530

    feat: update server bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import userRoutes from "./routes/user.routes";
+import firmRoutes from "./routes/firm.routes";
+import candidateRoutes from "./routes/candidate.routes";
+import adminRoutes from "./routes/admin.routes";
 
 const app = express();
 const port = 8080;
@@ -11,10 +14,13 @@ app.use(express.json());
 
 // Routes
 app.use("/api", userRoutes);
+app.use("/api", firmRoutes);
+app.use("/api", candidateRoutes);
+app.use("/api", adminRoutes);
 
 // Root route
 app.get("/", (req: Request, res: Response) => {
-  res.send("CA Worker API Server");
+  res.send("S K ASSOCIATES - CA Worker API Server");
 });
 
 // Start server
